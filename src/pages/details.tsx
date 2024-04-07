7dab2780a2c3fa51ef2bd759b9cba37c47848d33
meyricks <abrahamer122@gmail.com>
2024-04-07 06:25:16 -0400

    Added: some details structure in the tsx foundation

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -4,16 +4,18 @@ import { useParams } from 'react-router-dom';
 import { getRealEstatesById } from '../hooks/apiRequest'; 
 import { InfoProps } from "../utils/infoProps";
 import { useState, useEffect } from 'react';
+import styles from '../styles/pages/details.module.scss'
+import Button from "../components/ui/Button";
 
 const Details: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const [realEstateDetails, setRealEstateDetails] = useState<InfoProps | null>(null);
+  const [info, setInfo] = useState<InfoProps | null>(null);
 
   useEffect(() => {
     async function fetchDetails() {
       try {
         const apiResponse = await getRealEstatesById(Number(id)); // Convertimos el id a número y lo pasamos a la función de la API
-        setRealEstateDetails(apiResponse.data);
+        setInfo(apiResponse.data);
       } catch (error) {
         console.error('Error fetching real estate details', error);
       }
@@ -22,20 +24,90 @@ const Details: React.FC = () => {
     fetchDetails();
   }, [id]); 
 
-  if (!realEstateDetails) {
+  if (!info) {
     return <div>Cargando...</div>; 
   }
 
   return (
     <>
-
-
     <Header showSearchLabel={false} showSelect ={false} />
 
-      <h1>{realEstateDetails.bedrooms}</h1>
+    <section className={styles.details}>
+
+      <h5>{info.bedrooms}</h5>
+      <p>{info.bedrooms}</p>
+
+      <article className={styles.icons}>
+
+      </article>
+      <hr />
+        <h5>Dirección</h5>
+        <p>Calle Wallaby</p>
+      <hr />
+
+      <h5>Características</h5>
+
+      <article className={styles.left}>
+        <h5>Techo</h5>
+        <p>Concreto</p>
+
+        <h5>Pisos</h5>
+        <p>Concreto</p>
 
 
-      <h1>Detalles del inmueble {id}</h1>
+        <h5>Marcos</h5>
+        <p>Concreto</p>
+
+        <h5>Gabinetes</h5>
+        <p>Concreto</p>
+
+        
+        <h5>Ventanas</h5>
+        <p>Concreto</p>
+      </article>
+
+      
+      <article className={styles.right}>
+        <h5>Mejoras</h5>
+        {/* create a list with ul  and li */}
+        <ul>
+          <li>Mejora 1</li>
+          <li>Mejora 2</li>
+          <li>Mejora 3</li>
+          <li>Mejora 3</li>
+          <li>Mejora 3</li>
+        </ul>
+
+        <h5>Puertas</h5>
+        <p>Concreto</p>
+
+        <h5>Closets</h5>
+        <p>Concreto</p>
+
+      </article>
+
+      <div className={styles['agent-container']}>
+        <img src="" alt="" />
+        <h5>Puertas</h5>
+        <p>Concreto</p>
+
+        <hr />
+        <p>Contacto:</p>
+        <hr />
+
+        <ul>
+          <li>Mejora 1</li>
+          <li>Mejora 2</li>
+          <li>Mejora 3</li>
+        </ul>
+        <Button text="WHATSAPP" backgroundColor="#39B44A" />
+
+
+      </div>
+
+     
+
+      </section>
 
       <Footer/>
 
