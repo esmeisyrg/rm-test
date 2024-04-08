d4baf5b1398844d0c7f5996c71779443e66aafe7
meyricks <abrahamer122@gmail.com>
2024-04-07 20:58:12 -0400

    Added: hamburguer menu and main responsive styles

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,58 +4,63 @@ import SearchLabel from '../ui/Search-label'
 import US from '../../assets/usa.svg'
 import ES from '../../assets/rd.svg'
 import { Link } from 'react-router-dom'
-
 import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
 import { MdOutlineCheckBox } from "react-icons/md";
-
+import { useState } from 'react';
 
 interface HeaderProps {
   showSearchLabel: boolean
   showSelect: boolean
 }
 
+
 const Header: React.FC<HeaderProps> = ({ showSearchLabel, showSelect = true }) => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleOverlayClick = () => {
+    setMenuOpen(false); 
+  };
+
   return (
     <header className={styles.header}>
-      <div  className={`${styles.container} ${showSearchLabel ? styles.withSearchLabel : styles.withoutSearchLabel}`}>
+      <div className={`${styles.container} ${showSearchLabel ? styles.withSearchLabel : styles.withoutSearchLabel}`}>
         <Link to="/">
           <div className={styles.logo}>
             <img src={Logo} alt="logo" />
           </div>
         </Link>
 
-
         {showSearchLabel && <SearchLabel />}
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Comprar</a>
-            </li>
-            <li>
-              <a href="/about">Alquiler</a>
-            </li>
-            <li>
-              <a href="/contact">Agencia/Agentes</a>
-            </li>
-            <li>
-              <a href="/contact">Blog</a>
-            </li>
-            <li>
-              <a href="/contact">Contáctanos</a>
-            </li>
+        <nav className={styles.navigation}>
+          {menuOpen && <div className={styles.menuOverlay} onClick={handleOverlayClick}></div>}
+
+          <input
+            type="checkbox"
+            className={styles['toggle-menu']}
+            checked={menuOpen}
+            onChange={() => setMenuOpen(!menuOpen)}
+          />
+
+          <div className={styles.hamburguer}></div>
+
+          <ul className={styles.menu}>
+            <li><a href="/">Comprar</a></li>
+            <li><a href="/about">Alquiler</a></li>
+            <li><a href="/contact">Agencia/Agentes</a></li>
+            <li><a href="/contact">Blog</a></li>
+            <li><a href="/contact">Contáctanos</a></li>
+
             <hr className={styles.line}/>
             <li className={styles.lan}>IDIOMA
-              <a href="/contact"><img src={US} alt="US" /></a>
-              <a href="/contact"><img src={ES} alt="ES" /></a>
+              <a className={styles.lanI} href="/contact"><img src={US} alt="US" /></a>
+              <a className={styles.lanI} href="/contact"><img src={ES} alt="ES" /></a>
             </li>
             <hr className={styles.line}/>
-
-
           </ul>
         </nav>
       </div>
 
-      
       {showSelect && (
         <article className={styles['select-container']}>
           <div className={styles.buy}>
@@ -69,7 +74,7 @@ const Header: React.FC<HeaderProps> = ({ showSearchLabel, showSelect = true }) =
         </article>
       )}
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- src/components/property-card.tsx
+++ src/components/property-card.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect} from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import styles from '../styles/UI/property-card.module.scss';
 import IBath from '../assets/icons/Ibath.svg';
 import IRoom from '../assets/icons/IRoom.svg';
@@ -10,11 +10,8 @@ import HeartActive from '../assets/icons/savedActive.svg';
 import { Link } from 'react-router-dom';
 import { InfoProps } from '../utils/infoProps';
 
-
-
 const PropertyCard: React.FC = () => {
   const [data, setData] = useState<InfoProps[] | null>(null);
-
   const [favorites, setFavorites] = useState<number[]>([]);
 
   const toggleFavorite = (id: number) => {
@@ -24,74 +21,62 @@ const PropertyCard: React.FC = () => {
       setFavorites([...favorites, id]);
     }
   };
-  
 
   useEffect(() => {
-      async function fetchData() {
-          try {
-              const apiResponse = await getRealEstates();
-              console.log(apiResponse); 
-              const apiData = apiResponse.data; 
-              setData(apiData);
-          } catch (error) {
-              console.error('Error fetching real estates', error);
-          }
+    async function fetchData() {
+      try {
+        const apiResponse = await getRealEstates();
+        setData(apiResponse.data);
+      } catch (error) {
+        console.error('Error fetching real estates', error);
       }
-      fetchData();
+    }
+    fetchData();
   }, []);
+
+  const memoizedData = useMemo(() => data, [data]);
 
   return (
     <>
-      {data &&
-          data.map((item) => (       
-        <div key={item.id} className={styles.container}>
-          <article className={styles['card-container']}>
-            <div className={styles.card}>
-            <img
+      {memoizedData &&
+        memoizedData.map((item) => (
+          <div key={item.id} className={styles.container}>
+            <article className={styles['card-container']}>
+              <div className={styles.card}>
+                <img
                   className={styles.heart}
                   src={favorites.includes(item.id) ? HeartActive : Heart}
                   alt="Favorite"
                   onClick={() => toggleFavorite(item.id)}
                 />
-
-              <Link to={`/details/${item.id}`}>
+                <Link to={`/details/${item.id}`}>
                   <img className={styles.image} src={item.main_picture.small} alt="House" />
-              </Link>
-
-              <div className={styles['card-content']}>
-          
-                <h3 className={styles['property-title']}>{item.realstate_type}</h3>
-                <p className={styles.city}>{item.city}</p>
-                <p className={styles['city-sector']}>{item.city}, {item.sector}</p>
-                
-                <article className={styles['venta-container']}>
-                  <p className={styles.venta}>VENTA</p>
-                  <p className={styles.price}>$US {item.price}</p>
-
-                </article>
-                
-
-                <article className={styles.icons}>
-                  <img src={IParking} alt="Parking Icon" />
-                  <span>{item.parking_spots}</span>
-                  <img src={IBath} alt="Bathrooms icon" />
-                  <span>{item.bathrooms}</span>
-                  <img src={IRoom} alt="Room Icon" />
-                  <span>{item.bedrooms}</span>     
-                  <img src={IMetrics} alt="Metrics Icon" />
-                  <span>{item.sqm_land}</span> 
-                </article>
-                <p className={styles.code}>Cod.{item.id}</p>
-
+                </Link>
+                <div className={styles['card-content']}>
+                  <h3 className={styles['property-title']}>{item.realstate_type}</h3>
+                  <p className={styles.city}>{item.city}</p>
+                  <p className={styles['city-sector']}>{item.city}, {item.sector}</p>
+                  <article className={styles['venta-container']}>
+                    <p className={styles.venta}>VENTA</p>
+                    <p className={styles.price}>$US {item.price}</p>
+                  </article>
+                  <article className={styles.icons}>
+                    <img src={IParking} alt="Parking Icon" />
+                    <span>{item.parking_spots}</span>
+                    <img src={IBath} alt="Bathrooms icon" />
+                    <span>{item.bathrooms}</span>
+                    <img src={IRoom} alt="Room Icon" />
+                    <span>{item.bedrooms}</span>
+                    <img src={IMetrics} alt="Metrics Icon" />
+                    <span>{item.sqm_land}</span>
+                  </article>
+                  <p className={styles.code}>Cod.{item.id}</p>
+                </div>
               </div>
-            </div>
-
-          </article>
-
-    </div>
-    ))}
+            </article>
+          </div>
+        ))}
     </>
-
   );
 };
 
